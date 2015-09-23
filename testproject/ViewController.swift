c80623768bdf4a8dc0199f2122a9019496982689
BeokayTakuro <takuro@beokay.com>
2015-09-23 16:17:38 -0400

    first commit

---
+++ testproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testproject
+//
+//  Created by Boksolutions on 9/23/15.
+//  Copyright © 2015 Boksolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
